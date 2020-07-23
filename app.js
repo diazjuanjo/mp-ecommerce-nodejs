2ce2252beb2f6c8dfb368c20a0ba30d42e36be7c
diazjuanjo <diazjuanjose@icloud.com>
2020-07-23 09:42:21 -0300

    npm run build

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var app = express();
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+app.use(express.static('assets'));
+ 
+app.use('/assets', express.static(__dirname + '/assets'));
+
 app.get('/', function (req, res) {
     res.render('home');
 });
@@ -14,9 +18,5 @@ app.get('/', function (req, res) {
 app.get('/detail', function (req, res) {
     res.render('detail', req.query);
 });
-
-app.use(express.static('assets'));
- 
-app.use('/assets', express.static(__dirname + '/assets'));
  
 app.listen(port);
